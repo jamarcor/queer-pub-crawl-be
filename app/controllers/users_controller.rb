a98ccc78948b28272e64f3be1b1528eec2603ed3
James Correa <james.correa9090@gmail.com>
2022-02-03 09:30:01 -1000

    added jwt auth stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,18 @@
 class UsersController < ApplicationController
+    skip_before_action :require_login, only: [:create]
     def create
-        user = User.create(user_params), status: :ok
+        user = User.create(user_params)
+        if user.valid?
+            payload = { user_id: user.id }
+            token = encode_token(payload)
+            render json: { user: user, jwt: token }, status: :ok
+        else
+            render json: { errors: user.errors.full_messages }, status: :not_acceptable
     end
 
     private
 
     def user_params
-        params.require(:username).permit(:password)
+        params.permit(:username).permit(:password)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,13 @@ Rails.application.routes.draw do
   resources :bars, only: [:index, :show]
   resources :reviews, only: [:index, :show, :create, :delete]
   resources :users, only: [:create]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  post "/login", to: "auth#login"
+
+  get "/login", to: "auth#login"
+
+  get "/auto_login", to: "auth#auto_login"
+
+  get "/user_is_authed", to: "auth#user_is_authed"
+
 end
